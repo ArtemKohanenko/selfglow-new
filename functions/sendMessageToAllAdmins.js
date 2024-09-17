8418c7793ac877be4d5b298b4a1549f2ea9702ba
ArtemKohanenko <kartem0255@gmail.com>
2024-09-17 14:30:25 +0600

    Remove test admin

--- functions/sendMessageToAllAdmins.js
+++ functions/sendMessageToAllAdmins.js
@@ -1,17 +1,11 @@
 import { User } from '../models/User.js'
 
 export async function sendMessageToAllAdmins(ctx, message) {
-    // const admins = await User.findAll({
-    //     where: {
-    //         isAdmin: true
-    //     }
-    // });
-    // TODO: верни как было
-    const admins = [
-        {
-            tgId: 1780484769
+    const admins = await User.findAll({
+        where: {
+            isAdmin: true
         }
-    ]
+    });
     
     for (const admin of admins) {
         await ctx.api.sendMessage(admin.tgId, message, { parse_mode: 'HTML' });
