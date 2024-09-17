382f739a6910bd91baacc0155e70b580f86f4177
ArtemKohanenko <kartem0255@gmail.com>
2024-09-17 14:25:19 +0600

    Add call payment notification

--- actions/showPublicOffer.js
+++ actions/showPublicOffer.js
@@ -1,12 +1,15 @@
 import { Composer, InlineKeyboard } from 'grammy'
 import { Tarif } from './../models/Tarif.js'
 import { Payment } from './../models/Payment.js'
+import { User } from '../models/User.js'
+import { sendMessageToAllAdmins } from '../functions/sendMessageToAllAdmins.js'
 import 'dotenv/config'
 
 const composer = new Composer()
 
 
 composer.callbackQuery(/showPublicOffer/, async ctx => {
+	const user = await User.findOne({ where: { tgId: ctx.from.id } })
 	const tarifId = ctx.callbackQuery.data.split(' ')[1]
 	const tarif = await Tarif.findByPk(tarifId)
 	const payment = await Payment.create({
@@ -32,6 +35,11 @@ https://disk.yandex.ru/i/PYyrpIQmKf8WGg`, {
 			reply_markup: inline,
 			disable_web_page_preview: true
 		})
+
+	const messageToAdmins = `Пользователь: ${user.fullName ? `<a href="https://t.me/${user.username.replace('@', '')}">${user.fullName}</a>` : user.username} вызвал оплату тарифа ${tarif.name}
+Сумма: ${tarif.price} ${tarif.currency}
+`
+	await sendMessageToAllAdmins(ctx, messageToAdmins)
 })
 
 export default composer
--- functions/sendMessageToAllAdmins.js
+++ functions/sendMessageToAllAdmins.js
@@ -1,11 +1,17 @@
 import { User } from '../models/User.js'
 
 export async function sendMessageToAllAdmins(ctx, message) {
-    const admins = await User.findAll({
-        where: {
-            isAdmin: true
+    // const admins = await User.findAll({
+    //     where: {
+    //         isAdmin: true
+    //     }
+    // });
+    // TODO: верни как было
+    const admins = [
+        {
+            tgId: 1780484769
         }
-    });
+    ]
     
     for (const admin of admins) {
         await ctx.api.sendMessage(admin.tgId, message, { parse_mode: 'HTML' });
