b91dccf256d0397dd295ace3fa5f4cb08f2e820b
ArtemKohanenko <kartem0255@gmail.com>
2024-09-18 18:14:51 +0600

    Return admins list

--- functions/sendMessageToAllAdmins.js
+++ functions/sendMessageToAllAdmins.js
@@ -1,15 +1,11 @@
 import { User } from '../models/User.js'
 
 export async function sendMessageToAllAdmins(ctx, message) {
-    // const admins = await User.findAll({
-    //     where: {
-    //         isAdmin: true
-    //     }
-    // });
-    // TODO: раскомментируй выше, удали ниже
-    const admins = [{
-        tgId: 1780484769
-    }]
+    const admins = await User.findAll({
+        where: {
+            isAdmin: true
+        }
+    });
     
     console.log("Admins:", admins.map(admin => admin.tgId))
     
